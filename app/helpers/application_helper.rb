3cf4883360e72176705b4820c97262f0aa08f02c
cohako <muri.ilo@hotmail.com>
2021-05-14 23:01:32 -0300

    Currency to BRL converter on helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def number_to_currency_brl (number)
+    number_to_currency number, 
+    unit: "R$", 
+    separator:',', 
+    delimiter: '.', 
+    format: "%u %n" 
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -7,10 +7,6 @@ class Course < ApplicationRecord
   validates_uniqueness_of :code, 
                           message: 'já está em uso'
 
-  def price_to_real
-    "R$ #{price.to_i},00"
-  end
-
   def date_to_metric
     enrollment_deadline.strftime('%d/%m/%Y')
   end
